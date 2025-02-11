147e8b15c2ff28a5e1165668694af016489605fe
Department-of-Earthsciences <earthsciencessucr1@gmail.com>
2025-02-11 23:01:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("schedule-content").innerHTML = `
+        <ul>
+            <li><b>March 28, 2025:</b> Registration, Inaugural Ceremony, Keynote Sessions</li>
+            <li><b>March 29, 2025:</b> Technical Sessions, Panel Discussions, Valedictory</li>
+        </ul>`;
+});
